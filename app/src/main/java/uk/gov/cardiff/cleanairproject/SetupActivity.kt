75650350f37f609af29814c3d4365bf6454b6eca
Jonathan Cole <JonathanPeterCole@gmail.com>
2019-03-02 14:58:10 +0000

    Added a setup activity

---
+++ app/src/main/java/uk/gov/cardiff/cleanairproject/SetupActivity.kt
@@ -0,0 +1,23 @@
+package uk.gov.cardiff.cleanairproject
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SetupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme_Setup)
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setup)
+        // Stop setup showing again
+        setSetupComplete()
+    }
+
+    private fun setSetupComplete() {
+        // Set the FirstTimeSetup completed preference
+        val sharedPref = getSharedPreferences("FirstTimeSetup", MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putBoolean("completed", true)
+        editor.apply()
+    }
+}
