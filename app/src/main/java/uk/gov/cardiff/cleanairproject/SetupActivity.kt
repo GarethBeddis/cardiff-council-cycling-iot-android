a19c1aeff8b96ab7a1cc6c8c0caf8e1f6e329fb0
Jonathan Cole <JonathanPeterCole@gmail.com>
2019-03-03 16:38:04 +0000

    Added WIP fragments to the setup activity

--- app/src/main/java/uk/gov/cardiff/cleanairproject/SetupActivity.kt
+++ app/src/main/java/uk/gov/cardiff/cleanairproject/SetupActivity.kt
@@ -2,13 +2,27 @@ package uk.gov.cardiff.cleanairproject
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import uk.gov.cardiff.cleanairproject.setup_fragments.LocationFragment
+import uk.gov.cardiff.cleanairproject.setup_fragments.WelcomeFragment
 
-class SetupActivity : AppCompatActivity() {
+class SetupActivity : AppCompatActivity(), WelcomeFragment.OnCompleteListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme_Setup)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_setup)
+
+        // Prepare the fragments
+        val welcomeFragment = WelcomeFragment()
+
+        val fragmentManager = supportFragmentManager
+
+        val transaction = fragmentManager.beginTransaction()
+
+        transaction.replace(R.id.fragment_container, welcomeFragment)
+
+        transaction.commit()
+
         // Stop setup showing again
         // setSetupComplete()
     }
@@ -19,5 +33,24 @@ class SetupActivity : AppCompatActivity() {
         val editor = sharedPref.edit()
         editor.putBoolean("completed", true)
         editor.apply()
+    }
+
+    override fun onCompleteListener() {
+        val locationFragment = LocationFragment()
+//        val slideRight = Slide(Gravity.RIGHT)
+//        slideRight.duration = 200
+//
+//        // This is a like locationFragment.setEnterTransition(slideRight) in Java
+//        locationFragment.enterTransition = slideRight
+
+        val fragmentManager = supportFragmentManager
+
+        val transaction = fragmentManager.beginTransaction()
+
+        // transaction.setCustomAnimations(R.anim.fade_in, R.anim.fade_out)
+
+        transaction.replace(R.id.fragment_container, locationFragment)
+
+        transaction.commit()
     }
 }
