820b26e22b5be4499816f31ddcbbc6580cf337ca
Gareth Beddis <garethbeddis@gmail.com>
2019-03-05 15:08:56 +0000

    Created user model and database helper classes ref: https://github.com/Android-Tutorials-Hub/login-register-sqlite-tutorial-Kotlin/tree/master/app/src/main/java/com/androidtutorialshub/loginregisterkotlin

--- app/src/main/java/uk/gov/cardiff/cleanairproject/model/User.kt
+++ app/src/main/java/uk/gov/cardiff/cleanairproject/model/User.kt
@@ -1,2 +1,5 @@
 package uk.gov.cardiff.cleanairproject.model
 
+
+// Ref: https://github.com/Android-Tutorials-Hub/login-register-sqlite-tutorial-Kotlin/blob/master/app/src/main/java/com/androidtutorialshub/loginregisterkotlin/model/User.kt
+data class User(val id: Int = -1, val name: String, val email: String, val password: String)
