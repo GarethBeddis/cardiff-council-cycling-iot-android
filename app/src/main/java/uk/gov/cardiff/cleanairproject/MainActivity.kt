40a2eb5feeb467f5ae50c9a41fedaeed62c27425
Macaulay-Reiss Christian <christianm@cardiff.ac.uk>
2019-03-10 13:13:27 +0000

    FIXED issue with not loading next Activty when menu item clicked. pair-programmed with Jon Cole

--- app/src/main/java/uk/gov/cardiff/cleanairproject/MainActivity.kt
+++ app/src/main/java/uk/gov/cardiff/cleanairproject/MainActivity.kt
@@ -1,8 +1,9 @@
 package uk.gov.cardiff.cleanairproject
-
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
@@ -10,54 +11,50 @@ import android.widget.PopupMenu
 import android.widget.Toast
 import be.rijckaert.tim.animatedvector.FloatingMusicActionButton
 import kotlinx.android.synthetic.main.activity_main.*
-
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         val playPauseFab = playPauseButton as FloatingMusicActionButton
         playPauseFab.changeMode(FloatingMusicActionButton.Mode.PLAY_TO_PAUSE)
-
         playPauseButton.setOnMusicFabClickListener(object : FloatingMusicActionButton.OnMusicFabClickListener {
             override fun onClick(view: View) {
                 //do stuff
             }
-
         })
+        // Menu Icon listener
+        menuIcon.setOnClickListener {
+                v -> showPopup(v)
+        }
     }
-
     fun showPopup(v: View) {
+        // Inflate
         val popup = PopupMenu(this, v)
         val inflater: MenuInflater = popup.menuInflater
         inflater.inflate(R.menu.menu_main, popup.menu)
+        // Set on click listener
+        popup.setOnMenuItemClickListener { item ->
+            when (item.itemId) {
+                R.id.about -> {
+                    Toast.makeText(this,"Test", Toast.LENGTH_LONG).show()
+                    true
+                }
+                R.id.settings -> {
+                    val intent = Intent(this, SettingsActivity::class.java)
+                    this.startActivity(intent)
+                    true
+                }
+                R.id.help -> {
+                    true
+                }
+                R.id.logout -> {
+                    true
+                }
+                else -> super.onOptionsItemSelected(item)
+            }
+        }
+        // Show the menu
         popup.show()
     }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return when (item.itemId) {
-            R.id.about -> {
-                Toast.makeText(this,"Test", Toast.LENGTH_LONG).show()
-                true
-            }
-
-            R.id.settings -> {
-                val intent = Intent(this, SettingsActivity::class.java)
-                this.startActivity(intent)
-                true
-            }
-
-            R.id.help -> {
-                true
-            }
-
-            R.id.logout -> {
-                true
-            }
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
 }
