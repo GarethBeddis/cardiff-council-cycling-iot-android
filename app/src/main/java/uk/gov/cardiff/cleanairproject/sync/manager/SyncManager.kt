58ad3d1535098f75f33310eaeff685ed5ae3cfc7
Jonathan Cole <JonathanPeterCole@gmail.com>
2019-04-01 18:31:39 +0100

    Implemented support for updated web sync

--- app/src/main/java/uk/gov/cardiff/cleanairproject/sync/manager/SyncManager.kt
+++ app/src/main/java/uk/gov/cardiff/cleanairproject/sync/manager/SyncManager.kt
@@ -1,18 +1,19 @@
 package uk.gov.cardiff.cleanairproject.sync.manager
 
 import android.content.Context
-import android.util.Log
+import android.support.v7.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
-import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import com.google.gson.Gson
 import org.json.JSONArray
+import org.json.JSONObject
 import uk.gov.cardiff.cleanairproject.BuildConfig
 import uk.gov.cardiff.cleanairproject.data.model.Journey
 import uk.gov.cardiff.cleanairproject.data.sql.DatabaseHelper
 
-class SyncManager(context: Context) {
+class SyncManager(private val context: Context) {
 
     private val databaseHelper = DatabaseHelper(context)
     private val serverAddress = BuildConfig.API_URL
@@ -28,16 +29,18 @@ class SyncManager(context: Context) {
 
     fun syncJourneys(listener: SyncListener) {
         // Get the unsynced journeys from the database and convert them to JSON
-        val requestJSON = Gson().toJson(databaseHelper.getUnsyncedJourneys())
+        val journeysJSON = Gson().toJson(databaseHelper.getUnsyncedJourneys())
+        val requestJSON = "{\"token\": \"${getToken()}\", \"journeys\": $journeysJSON}"
         // Prepare the JSON object
-        val jsonRequest = JsonArrayRequest(
+        val jsonRequest = JsonObjectRequest(
             Request.Method.POST,
             "$serverAddress/api/app/sync/journeys",
-            JSONArray(requestJSON),
-            Response.Listener<JSONArray> {response ->
+            JSONObject(requestJSON),
+            Response.Listener<JSONObject> {response ->
+                val responseJourneys = JSONArray(response.getString("journeys"))
                 val journeys = mutableListOf<Journey>()
-                for (i in 0 until response.length()) {
-                    journeys.add(Gson().fromJson(response[i].toString(), Journey::class.java))
+                for (i in 0 until responseJourneys.length()) {
+                    journeys.add(Gson().fromJson(responseJourneys[i].toString(), Journey::class.java))
                 }
                 databaseHelper.updateJourneys(journeys)
                 listener.onSyncSuccess()
@@ -51,16 +54,18 @@ class SyncManager(context: Context) {
 
     fun syncReadings(listener: SyncListener) {
         // Get the unsynced readings from the database and convert them to JSON
-        val requestJSON = Gson().toJson(databaseHelper.getUnsyncedReadings())
+        val readingsJSON = Gson().toJson(databaseHelper.getUnsyncedReadings())
+        val requestJSON = "{\"token\": \"${getToken()}\", \"readings\": $readingsJSON}"
         // Prepare the JSON object
-        val jsonRequest = JsonArrayRequest(
+        val jsonRequest = JsonObjectRequest(
             Request.Method.POST,
             "$serverAddress/api/app/sync/readings",
-            JSONArray(requestJSON),
-            Response.Listener<JSONArray> {response ->
+            JSONObject(requestJSON),
+            Response.Listener<JSONObject> {response ->
+                val responseReadings = JSONArray(response.getString("readings"))
                 val readingIDs = mutableListOf<Int>()
-                for (i in 0 until response.length()) {
-                    readingIDs.add(response.getInt(i))
+                for (i in 0 until responseReadings.length()) {
+                    readingIDs.add(responseReadings.getInt(i))
                 }
                 databaseHelper.updateReadings(readingIDs)
                 listener.onSyncSuccess()
@@ -70,5 +75,11 @@ class SyncManager(context: Context) {
             })
         // Make the request
         requestQueue.add(jsonRequest)
+    }
+
+    private fun getToken(): String {
+        // Get the token from shared prefs
+        return context.getSharedPreferences("Account", AppCompatActivity.MODE_PRIVATE)
+            .getString("token", "")!!
     }
 }
